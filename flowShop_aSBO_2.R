5469f2311d6e239fbed775dcaa0e3f9de9767d8d
Ícaro Agostino <icaroagostino@gmail.com>
2020-03-25 02:57:23 +0100

    Update flowShop_aSBO_2.R

--- flowShop_aSBO_2.R
+++ flowShop_aSBO_2.R
@@ -77,7 +77,9 @@ seed <- 999           # Semente para garantir reprodução
 
 lower <- c(1,1,1,1,1) # Restrições de min
 upper <- c(5,5,5,5,5) # Restrições de max
-inicio <- Sys.time()
+                  
+inicio <- Sys.time()  # Tempo de início da otimização
+                  
 # Otimizacao em GA para encontrar a combinacao otima de recursos
 GA <- ga(type = "real-valued",
          fitness = function(x) simular(x[1],x[2],x[3],x[4],x[5]),
@@ -90,8 +92,10 @@ GA <- ga(type = "real-valued",
          pmutation = pmutation,
          parallel = parallel,
          seed = seed)
-fim <- Sys.time()
-fim-inicio
+                  
+fim <- Sys.time()   # TEmpo de fim da simulação
+fim-inicio          # Tempo total de simulação
+                  
 # Melhor solucao
 solution <- round(GA@solution)[1,] %>% print
 plot(GA) # Plota busca
